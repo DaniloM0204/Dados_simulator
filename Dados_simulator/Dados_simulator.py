ca438a6237d3219b6b6da7f2f220e26be71a66fd
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-02-02 20:15:39 -0300

    Adicionar arquivos de projeto.

---
+++ Dados_simulator/Dados_simulator.py
@@ -0,0 +1,11 @@
+# Simulador de dado
+
+import random 
+
+class SimulatorDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem = " Gostaria de um novo valor para o dado?"
+
+
