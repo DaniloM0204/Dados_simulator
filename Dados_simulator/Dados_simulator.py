823569c70cb295df5f19a965a74e9134b0da7015
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-02-03 15:22:42 -0300

    Reparando e deixando como um Simulador de Dado

--- Dados_simulator/Dados_simulator.py
+++ Dados_simulator/Dados_simulator.py
@@ -1,40 +1,28 @@
 # Simulador de dado
 
-from _typeshed import Self
 import random 
-import PySimpleGUI
+
 
 class SimulatorDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        # Layout
-        layout = [
-               [sg.Text('Jogar o dado?')],
-               [sg.Button('S'),sg.Button('N')]
-        ]
+        self.mensagem = "Voce gostaria de gerar um novo valor para o dado?"
 
     def Iniciar(self):
-
-        # Criar uma janela
-        self.janela = sg.Windoe("Simulador de Dado", Layout=layout)
-        # Ler valores
-        self.eventos, self.valores = janela.Read()
-        # Fazer algo com valores
-        while True:
-            try:
-                if self.eventos == 'S':
-                    self.GerarValorDoDado()
-                elif self.eventos == 'N':
-                    print("Obrigado pela participação")
-                else:
-                    print("Digite S ou N")
-            except:
-                print("Ocorreu um erro ao receber sua resposta")
+        self.eventos = input(self.mensagem)
+        try:
+            if self.eventos == 'S':
+                self.GerarValorDoDado()
+            elif self.eventos == 'N':
+                print("Obrigado pela participaçao")
+            else:
+                print("Digite S ou N")
+        except:
+             print("Ocorreu um erro ao receber sua resposta")
 
     def GerarValorDoDado(self):
         print(random.randomint(self.valor_minimo,self.valor_maximo))
-
 
 simulador = SimulatorDado
 simulador.Iniciar()
