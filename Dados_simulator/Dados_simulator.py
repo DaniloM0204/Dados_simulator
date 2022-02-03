5e0563175c324132cd62c9dbb512d2b6d39f38bf
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-02-03 14:30:23 -0300

    Commit Dados Inicio

--- Dados_simulator/Dados_simulator.py
+++ Dados_simulator/Dados_simulator.py
@@ -1,11 +1,40 @@
 # Simulador de dado
 
+from _typeshed import Self
 import random 
+import PySimpleGUI
 
 class SimulatorDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = " Gostaria de um novo valor para o dado?"
+        # Layout
+        layout = [
+               [sg.Text('Jogar o dado?')],
+               [sg.Button('S'),sg.Button('N')]
+        ]
+
+    def Iniciar(self):
+
+        # Criar uma janela
+        self.janela = sg.Windoe("Simulador de Dado", Layout=layout)
+        # Ler valores
+        self.eventos, self.valores = janela.Read()
+        # Fazer algo com valores
+        while True:
+            try:
+                if self.eventos == 'S':
+                    self.GerarValorDoDado()
+                elif self.eventos == 'N':
+                    print("Obrigado pela participação")
+                else:
+                    print("Digite S ou N")
+            except:
+                print("Ocorreu um erro ao receber sua resposta")
+
+    def GerarValorDoDado(self):
+        print(random.randomint(self.valor_minimo,self.valor_maximo))
 
 
+simulador = SimulatorDado
+simulador.Iniciar()
